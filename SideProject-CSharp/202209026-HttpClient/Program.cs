7b4a39ed3f10a5b9b44b751dcb60ebcd2ed62d2a
CECI\63496 <sunshineyen@g.ncu.edu.tw>
2022-09-26 14:05:01 +0800

    20220926 HttpClient (實作 Using GetAsync)

---
+++ SideProject-CSharp/202209026-HttpClient/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    private const string uri = "http://api.geonames.org/search";
+
+    static void Main(string[] args)
+    {
+        Run();
+
+        Console.WriteLine("Hit enter to exit...");
+        Console.ReadKey();
+    }
+
+    static async void Run()
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                client.Timeout = TimeSpan.FromSeconds(30);
+                HttpResponseMessage response = await client.GetAsync(uri);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                Console.WriteLine(responseBody);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+        }
+    }
+}
